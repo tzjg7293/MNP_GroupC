8c3810b1675722132d212110670d46ab29f74508
tzjg7293 <sydney.tj.fang@gmail.com>
2022-11-24 17:35:57 -0800

    pulled initial.py updated from dev

--- DjangoProject/Project/migrations/0001_initial.py
+++ DjangoProject/Project/migrations/0001_initial.py
@@ -20,19 +20,13 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
             ],
-            options={
-                'verbose_name_plural': 'Organizations',
-            },
         ),
         migrations.CreateModel(
             name='OrgGroups',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('group', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='Project.organizations')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('groupId', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='Project.organizations')),
+                ('userId', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
-            options={
-                'verbose_name_plural': 'OrgGroups',
-            },
         ),
     ]
